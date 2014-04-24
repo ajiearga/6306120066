138dbbdfec976f091f6bea76fae3a04c572a5bd8
Ajie Pramono Arganata <ajie.pramono.arganata@gmail.com>
2014-04-24 17:12:07 +0700

    Sum of Fibonacci
    Modifikasi jumlah dari fibonacci

---
+++ fibonaccisum.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package fibonaccisum;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ *
+ * @author Asisten
+ */
+public class Fibonaccisum {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        try {
+            // TODO code application logic here
+            System.out.print("Input : ");
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            int angka=Integer.parseInt(br.readLine());
+            int aw=1;
+            int hsl=0;
+            int temp=0;
+            int total=0;
+            for(int i=0;i<angka;i++){
+                if(i==0 || i==1){
+                    hsl=aw;
+                }else if(i>1){
+                    hsl=hsl+temp;
+                }
+                total=total+hsl;
+                temp=aw;
+                aw=hsl;
+            }
+            System.out.println("");
+            System.out.println("");
+            System.out.println("Output : "+total);
+        } catch (IOException ex) {
+            System.out.println("System err "+ex.getMessage());;
+        }
+    }
+    
+}
